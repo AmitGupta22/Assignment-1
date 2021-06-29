cfbd0b203983c70c642d4786e40dd9d8424b3198
AmitGupta22 <86664205+AmitGupta22@users.noreply.github.com>
2021-06-29 17:40:35 +0530

    Day 1 Assignment Submitted

---
+++ Assignment 1.py
@@ -0,0 +1,8 @@
+a = float(input('Enter 1st number:'))
+b = float(input('Enter 2nd number:'))
+sub = a-b
+print('your subtraction is: ',sub)
+mul = a*b
+print('your multiplication is: ',mul)
+div = a/b
+print('Your division is: ',div)
